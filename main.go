43a81023f7a8487fe8c005ad09f9417f64d3ceb0
Galen Guyer <galen@galenguyer.com>
2021-07-09 08:14:06 -0700

    oops all localhost

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/miekg/dns"
+)
+
+type handler struct{}
+
+func (this *handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
+	msg := dns.Msg{}
+	msg.SetReply(r)
+	switch r.Question[0].Qtype {
+	case dns.TypeA:
+		msg.Authoritative = true
+		domain := msg.Question[0].Name
+		msg.Answer = append(msg.Answer, &dns.A{
+			Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
+			A:   net.ParseIP("127.0.0.1"),
+		})
+	}
+	w.WriteMsg(&msg)
+}
+
+func main() {
+	srv := &dns.Server{Addr: "127.0.0.1:53", Net: "udp"}
+	srv.Handler = &handler{}
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to set udp listener %s\n", err.Error())
+	}
+}
